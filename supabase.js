b621fcd2b1899955644f0370805d7fb092944ea3
Ramsfield30 <ayibakeperemo476@gmail.com>
2026-03-06 12:44:03 +0100

    update

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'https://qmcjuwmqicdbuefxvfta.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtY2p1d21xaWNkYnVlZnh2ZnRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2Mjc1MzEsImV4cCI6MjA4ODIwMzUzMX0.CeLSsqV6m69N_48MiFibXGk-NRDuByweiczE5NN6FEs';
+const SUPABASE_URL = 'https://gjdnkxonwdncksabmzty.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqZG5reG9ud2RuY2tzYWJtenR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NDI5NzYsImV4cCI6MjA4ODMxODk3Nn0.zECDi6zm776FPM5MWFq3QpzgbZ9f9FLh8tIli74aokY';
 
 const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_KEY);
